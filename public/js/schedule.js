524ef32231c48a84f5c563f98422f9d6ad2b1532
ctgscott <ctgscott@hotmail.com>
2013-12-09 19:04:53 -0800

    blah...

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,6 +1,6 @@
-var geocoder;
-var day;
-var markers = [];
+//var geocoder;
+//var day;
+//var markers = [];
 /*var LocationData = [
 	[49.2812668, -123.1035942, "26 E Hastings St, Vancouver" ], 
 	[49.2814064, -123.1025187, "71 E Hastings St, Vancouver" ], 
@@ -9,7 +9,7 @@ var markers = [];
 	[49.2811625, -123.0985032, "242 E Hastings St, Vancouver" ],
 	[49.2811625, -123.0935032, "344 E Hastings St, Vancouver" ]
 ]; */
-var LocationData = [["2544 3rd Street, Santa Monica, CA 90405",	"2314 26th St., Santa Monica, CA  90405"], ['2401 29th St., Santa Monica, CA  90405', '2436 33rd St., Santa Monica, CA  90405']];
+//var LocationData = [["2544 3rd Street, Santa Monica, CA 90405",	"2314 26th St., Santa Monica, CA  90405"], ['2401 29th St., Santa Monica, CA  90405', '2436 33rd St., Santa Monica, CA  90405']];
 /*	[
 		"2544 3rd Street, Santa Monica, CA 90405",	
 		"2314 26th St., Santa Monica, CA  90405"
@@ -35,16 +35,85 @@ var LocationData = [["2544 3rd Street, Santa Monica, CA 90405",	"2314 26th St., 
 
 
 
-$(document).ready(function(){
+/*$(document).ready(function(){
 	initMap(1);  
-/*	initMap(2);  
+	initMap(2);  
 	initMap(3);  
 	initMap(4);  
-	initMap(5);  */
+	initMap(5);  
 });
 
 function initMap(id) {
-	geocoder = new google.maps.Geocoder();
+
+	$("#map_"+id).goMap({
+		zoom:6,
+		maptype: 'ROADMAP',
+		scaleControl: true
+	});
+
+	$.get('/positions.json', function(data) {
+		for(var i = 0, l = data.positions.length; i < l; i++) {
+			var marker = data.positions[i];
+
+			$.goMap.createMarker({
+				latitude: marker.latitude,
+				longitude: marker.longitude,
+				title: marker.device_label
+			});
+			
+			$.goMap.fitBounds();
+		}
+	}, 'json');
+	
+	$(this).attr("disabled","disabled");
+};
+*/
+
+$(document).ready(function(){
+	var myMaps = [];
+	
+	function buildMap(n) {
+	//	alert(n);
+		return function() {
+			$("#map_"+n).goMap({
+				zoom:6,
+				maptype: 'ROADMAP',
+				scaleControl: true
+			});
+
+			$.get('/positions'+n+'.json', function(data) {
+				for(var i = 0, l = data.positions.length; i < l; i++) {
+					var marker = data.positions[i];
+
+					$.goMap.createMarker({
+						latitude: marker.latitude,
+						longitude: marker.longitude,
+						title: marker.device_label
+					});
+					
+					$.goMap.fitBounds();
+				}
+				
+		//			alert(n);
+			}, 'json');
+			
+			//$(this).attr("disabled","disabled");
+		};
+	}
+	
+	for (n = 1; n < 6; n++) {
+		myMaps[n] = buildMap(n);
+	}
+	
+	for (var j = 1; j < 6; j++) {
+		myMaps[j]();
+	}
+});
+
+
+
+
+/*	geocoder = new google.maps.Geocoder();
 		 
 	var map = new google.maps.Map(document.getElementById('map_'+id));
 	var bounds = new google.maps.LatLngBounds();
@@ -58,8 +127,30 @@ function initMap(id) {
 		for (var n in LocationData[i])
 		{
 			alert('p['+n+'] = '+p[n]);
-			var latlng = codeAddress(p[0]);
+			var latlng = new google.maps.LatLng(0, 0);
+/*			latlng = geocoder.geocode( { 'address': p[n]}, function(results, status) {
+				if (status == google.maps.GeocoderStatus.OK) {
+					alert('results = '+results[0].geometry.location);
+					alert('results.lat() = '+results[0].geometry.location.lat());
+					alert('results.lng() = '+results[0].geometry.location.lng());
+//					return results[0].geometry.location;
+				} else {
+				alert("Geocode was not successful for the following reason: " + status);
+				}
+			});
+*/
+/*			var latlng = codeAddress(p[n], function(makeMarker) {;
+				var marker = new google.maps.Marker({
+					position: latlng,
+					map: map,
+					//title: p[2]
+				});
+			});
+//			var latlng = new google.maps.LatLng(codeAddress(p[0]).lat(), codeAddress(p[0]).lng());
+//			var latlng = codeAddress('2544 3rd Street, Santa Monica, CA  90405');
+//			var test = new google.maps.LatLng(latlng.lat(), latlng.lng());
 			alert('latlng = '+latlng);
+//			alert('test = '+test);
 			bounds.extend(latlng);
 
 			
@@ -82,17 +173,20 @@ function initMap(id) {
 	//codeAddress('2544 3rd Street, Santa Monica, CA 90274', 1);
 }
 
-function codeAddress(address) {
+function codeAddress(address, callback) {
     geocoder.geocode( { 'address': address}, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK) {
-			//alert('results = '+results[0].geometry.location);
-			return results[0].geometry.location;
+			alert('results = '+results[0].geometry.location);
+			alert('results.lat() = '+results[0].geometry.location.lat());
+			alert('results.lng() = '+results[0].geometry.location.lng());
+//			return results[0].geometry.location;
+			
 		} else {
         alert("Geocode was not successful for the following reason: " + status);
 		}
 	});
 }
-	
+*/	
 /*$(document).ready(function(){
 
 	google.maps.event.addDomListener(window, 'load', initialize);
